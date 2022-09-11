69dafbca4f23959ff976171706db3067eeb9e4c1
kerby <lovincekerby99@gmail.com>
2022-09-11 20:02:05 -0300

    add

---
+++ Node/process.js
@@ -0,0 +1,6 @@
+// console.log(process.argv)
+//enter node process + o nome
+const firstName = process.argv[2]
+const lastName = process.argv[3]
+
+console.log(`Seu nome é ${firstName} ${lastName}`)
