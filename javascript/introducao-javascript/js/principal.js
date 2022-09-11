3a55eff8d2f72676f26e89edefef292ba33937b2
kerby <lovincekerby99@gmail.com>
2022-09-11 19:23:36 -0300

    commit

--- javascript/introducao-javascript/js/principal.js
+++ javascript/introducao-javascript/js/principal.js
@@ -30,3 +30,104 @@ if (pesoEhValido && alturaEhValida) {
     var imc = peso / (altura * altura);
     tdImc.textContent = imc;
 }
+
+// essa parte é para calcular o imc de tods os colunas
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length ; i++){
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
+    }
+
+    if(altura <= 0 || altura >= 3.0){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+    }
+
+    if(pesoEhValido && alturaEhValida){
+        var imc = peso / ( altura * altura);    
+        tdImc.textContent = imc;
+    } 
+    
+    // quantidade de caso decimal no toFixed(2)
+
+        if(pesoEhValido && alturaEhValida){
+            var imc = peso / ( altura * altura);    
+            //ALTERAÇÃO AQUI...
+            tdImc.textContent = imc.toFixed(2);
+        }
+    }       
+
+    // adicionar um novo paciente
+
+
+    var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona"); // Pegando as funcoes do form
+
+    var nome = form.nome.value;      // pegando os valores do form
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr"); // create tabela
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calculaImc(peso,altura);
+
+
+    pacienteTr.appendChild(nomeTd);  // adicionar os valores na tabela tr
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    // adicionar o paciente na tabela
+    var tabela = document.querySelector("#tabela-pacientes");  // select a tabela principal que e body
+
+    tabela.appendChild(pacienteTr);   // adicionar na tabela principal
+
+    //
+
+// restante do código calcula-imc.
+
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+
+    
+if (pesoEhValido && alturaEhValida) {
+    var imc = calculaImc(peso,altura);
+    imcTd.textContent = imc;
+}
+}
+});
+
+
