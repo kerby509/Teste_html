2e3b8e360289d728cae3df37d4fcdb9baa76297f
kerby <lovincekerby99@gmail.com>
2022-09-29 13:49:28 -0300

    add

--- Node/modules/fs.js
+++ Node/modules/fs.js
@@ -2,12 +2,33 @@ const fs = require ('fs');
 const path = require('path');
 
 // criar uma pasta
-//  fs.mkdir(path.join(__dirname, "/test"), (error) =>{
+ fs.mkdir(path.join(__dirname, "/test"), (error) =>{
 
-//     if (error){
-//       return  console.log('Erro',error)  
-//     }
+    if (error){
+      return  console.log('Erro',error)  
+    }
 
-//     console.log("Pasta foi criado com sucesso!!!")
+    console.log("Pasta foi criado com sucesso!!!")
 
-//  })
+ })
+
+
+// criar um arquivo
+
+fs.writeFile(path.join(__dirname, '/test','test.txt'), 'Hello world!!', (error) =>{
+    if (error){
+        return console.log("ERRO", error);
+    }
+
+    console.log("Conteudo addicionado com sucesso!!!!")
+})
+
+// addicionar um arquivo
+
+fs.appendFile(path.join(__dirname, '/test','test.txt'), 'Hello  Node!!', (error) =>{
+    if (error){
+        return console.log("ERRO", error);
+    }
+
+    console.log("Arquivo modificado com sucesso!!!!")
+})
