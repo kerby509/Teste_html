8155819538ae213bbbfd139847199ef141330a3c
kerby <lovincekerby99@gmail.com>
2022-10-05 13:41:46 -0300

    add

--- Node/modules/http.js
+++ Node/modules/http.js
@@ -7,5 +7,23 @@ const server =http.createServer((req, res) =>{
         res.end("<h1>Home page, Testando node js</h1>");
     }
 
-})
+    if ( req.url === '/users'){
+        const users= [
+            {
+                name: 'John Doe',
+                email: 'john@doe.com',
+            },
+            {
+                name: 'John Doe',
+                email: 'john@doe.com',
+
+            },
+        ];
+
+        res.writeHead(200, { "content-Type": "aplication/json"});
+        res.end(JSON.stringify(users));
+
+    }
+
+});
 server.listen(port, () => console.log(`Rodando na porta ${port}!!`));
