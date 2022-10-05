5a42b37cb47738b97d3a098a4add782a1c3e1df3
kerby <lovincekerby99@gmail.com>
2022-10-05 13:16:49 -0300

    add

--- Node/modules/http.js
+++ Node/modules/http.js
@@ -1,1 +1,11 @@
 const http=require ('http')
+
+const port = 8080;
+const server =http.createServer((req, res) =>{
+    if (req.url === "/home"){
+        res.writeHead(200, { "content-Type": "text/html"});
+        res.end("<h1>Home page</h1>");
+    }
+
+})
+server.listen(port, () => console.log(`Rodando na porta ${port}!!`));
